c74dc304139b67f7351ea5c9c673249be64bc523
DESKTOP-RFHFMFO\Mostafa <mostafahekmatiii22@gmail.com>
2023-05-20 10:42:32 +0330

    versioning

---
+++ app/Http/Controllers/API/V1/UsersController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+
+}
---
+++ tests/API/V1/UsersTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace API\V1;
+
+use Tests\TestCase;
+
+class UsersTest extends TestCase
+{
+
+}
